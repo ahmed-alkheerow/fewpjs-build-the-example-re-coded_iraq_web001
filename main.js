aa8dbed2593a665da466e764550b44ef2fcf02e1
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-07-04 23:33:11 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
+// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-
-
 // Your JavaScript code goes here!
+
+
 document.addEventListener('DOMContentLoaded',function(e){
 
   let hide = document.getElementById('modal')
@@ -33,5 +34,19 @@ document.addEventListener('click', function(e){
 })
 })
 
-
 //------------------------------------------------------------------------------
+// Ignore after this point. Used only for demo purposes
+//------------------------------------------------------------------------------
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
